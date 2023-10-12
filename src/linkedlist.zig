1e3fd86c0cff98cce49cb7d513785708ab3618b5
Mawfyy <mawfyy12@gmail.com>
2023-10-12 18:13:54 -0500

    fix: improve code and file name

--- src/linkedlist.zig
+++ src/linkedlist.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const alloc = std.heap.GeneralPurposeAllocator(.{}){};
+var alloc = std.heap.GeneralPurposeAllocator(.{}){};
 const expect = std.testing.expect;
 const person = @import("function.zig");
 
